d4dd9a10897ec69732594daae36a820f725e87e2
Jordi Grados <jordi@latostadora.com>
2017-11-23 15:56:06 +0100

    Add Security to Soap Factory Response

--- src/klareNNNs/MRW/Services/SoapResponseFactory.php
+++ src/klareNNNs/MRW/Services/SoapResponseFactory.php
@@ -2,14 +2,48 @@
 
 namespace klareNNNs\MRW\Services;
 
+use UnexpectedValueException;
 use klareNNNs\MRW\Entity\Delivery;
 
 class SoapResponseFactory
 {
     public static function create($response): Delivery
     {
-        return new Delivery($response->TransmEnvioResult->Estado, $response->TransmEnvioResult->Mensaje,
-            $response->TransmEnvioResult->NumeroSolicitud, $response->TransmEnvioResult->NumeroEnvio,
-            $response->TransmEnvioResult->Url);
+        if (static::validateFields($response)) {
+            throw new UnexpectedValueException('The Response has unexpected Values');
+        }
+        $result = $result = $response->TransmEnvioResult;
+
+        return new Delivery(
+            $result->Estado,
+            $result->Mensaje,
+            $result->NumeroSolicitud,
+            $result->NumeroEnvio,
+            $result->Url
+        );
+    }
+
+    private static function validateFields($response): bool
+    {
+        if (isset($response->TransmEnvioResult)) {
+            $result = $response->TransmEnvioResult;
+            if (!isset($result->Estado)) {
+                return false;
+            }
+            if (!isset($result->Mensaje)) {
+                return false;
+            }
+            if (!isset($result->NumeroSolicitud)) {
+                return false;
+            }
+            if (!isset($result->NumeroEnvio)) {
+                return false;
+            }
+            if (!isset($result->Url)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

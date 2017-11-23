be6f0393846e716e11ff846940eafff899f20555
Albert Claret <albert.claret@latostadora.com>
2017-11-23 17:11:04 +0100

    tests log failure

--- tests/Integration/klareNNNs/MRW/ApiClientTest.php
+++ tests/Integration/klareNNNs/MRW/ApiClientTest.php
@@ -82,4 +82,62 @@ class IntegrationApiClientTest extends TestCase
         $this->assertInstanceOf('\klareNNNs\MRW\Entity\Delivery', $delivery);
     }
 
+    public function testApiClientCanNotLog()
+    {
+        $soap = new SoapClient(self::TEST_SOAP_CLIENT, array('trace' => 1, "exceptions" => 0));
+        $franchise = getenv('FRANCHISE');
+        $subscriber = getenv('SUBSCRIBER');
+        $department = getenv('DEPARTMENT');
+        $user = getenv('USER');
+        $password = '';
+        $auth = new AuthHeader($franchise, $subscriber, $department, $user, $password);
+
+        $apiClient = new ApiClient($soap, $auth);
+
+
+        $date = date("dd/mm/YY");
+        $reference = 'BWZXTFSZU';
+        $onFranchise = 'N';
+        $serviceCode = '0200';
+        $serviceDescription = '';
+        $items = '';
+        $numberOfItems = '1';
+        $weight = '1';
+        $saturdayDelivery = 'N';
+        $return = 'N';
+        $refund = 'O';
+        $refundAmount = '25,9';
+
+        $serviceData = new ServiceData($date, $reference, $onFranchise, $serviceCode, $serviceDescription, $items,
+            $numberOfItems, $weight, $saturdayDelivery, $return, $refund, $refundAmount);
+
+        $addressCode = '';
+        $viaType = '';
+        $via = 'Calle Ramon y Cajal';
+        $number = '22';
+        $other = '';
+        $postalCode = '08402';
+        $city = 'Granollers';
+        $countryCode = 'ESP';
+
+        $shippingAddress = new ShippingAddress($addressCode, $viaType, $via, $number, $other, $postalCode, $city,
+            $countryCode);
+
+        $nif = '';
+        $name = 'Jorge';
+        $telephone = '666666666';
+        $contact = 'Jorge';
+        $atentionTo = 'Jorge';
+        $observations = '';
+
+        $shippingUser = new ShippingUser($nif, $name, $telephone, $contact, $atentionTo, $observations);
+
+        $delivery = $apiClient->createTransaction($serviceData, $shippingAddress, $shippingUser);
+
+        $this->assertInstanceOf('\klareNNNs\MRW\ApiClient', $apiClient);
+        $this->assertInstanceOf('\klareNNNs\MRW\Entity\Delivery', $delivery);
+        $this->assertTrue($delivery->getMessage() == '1) El usuario especificado no dispone de acceso al sistema, consulte con su franquicia.');
+        $this->assertTrue($delivery->getState() == 0);
+    }
+
 }
